8d10234a31a686a9f46ef246fe68c186ed70625b
lznauy <lznauyfine@gmail.com>
2026-05-12 20:27:26 +0800

    fixed bug

--- home/base/secrets.nix
+++ home/base/secrets.nix
@@ -3,7 +3,7 @@
   xdg.configFile."opencode/config.json" = {
     source = config.lib.file.mkOutOfStoreSymlink "/run/agenix/opencode-json";
   };
-  xdg.configFile."claude/settings.json" = {
+  home.file.".claude/settings.json" = {
     source = config.lib.file.mkOutOfStoreSymlink "/run/agenix/claude-settings";
   };
 }
