d2de1f6368020bdb6d947526503dbc79555cc532
lznauy <lznauyfine@gmail.com>
2026-08-13 23:24:44 +0800

    增加注释

--- home/programs/open-design.nix
+++ home/programs/open-design.nix
@@ -8,6 +8,7 @@
   services.open-design = {
     enable = true;
     autoStart = true;
+    # Web 前端由 Caddy 提供，访问 http://127.0.0.1:5174/；7457 是 daemon API 端口。
     webFrontend.enable = true;
   };
 }
