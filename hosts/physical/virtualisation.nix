69c202fda686bc0bf16a8331c08402f7cad857a2
lznauy <lznauyfine@gmail.com>
2026-08-27 00:35:23 +0800

    fixed virt

--- hosts/physical/virtualisation.nix
+++ hosts/physical/virtualisation.nix
@@ -1,20 +1,8 @@
 { pkgs, ... }:
 
-let
-  # niri 全局 prefer-no-csd 会让 GTK 应用不画标题栏；强制 virt-manager 用 CSD 恢复关闭按钮
-  virt-manager-csd = pkgs.symlinkJoin {
-    name = "virt-manager-csd";
-    paths = [ pkgs.virt-manager ];
-    buildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/virt-manager --set GTK_CSD 1
-    '';
-  };
-in
 {
   programs.virt-manager = {
     enable = true;
-    package = virt-manager-csd;
   };
 
   virtualisation.libvirtd = {
